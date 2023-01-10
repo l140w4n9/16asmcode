53337d53e8d6bfb55cd2d06f96569e98612bdd0e
l140w4n9 <44871001+l140w4n9@users.noreply.github.com>
2023-01-10 19:18:39 +0800

    Create memcpy.asm

---
+++ 01串操作示例代码/memcpy.asm
@@ -0,0 +1,30 @@
+mystack segment stack
+    db 512 dup(0)
+mystack ends
+
+mydata segment
+    g_szStr1 db 11H, 22H, 33H, 44H, 55H, 66H, 77H, 88H, 99H, 0aaH, 0bbH, 0ccH, 0ddH, 0eeH, 0ffH
+    g_szStr2 db 16 dup(0)
+    ; g_szStr3 db 11H, 22H, 33H, 44H, 77H, 66H, 77H, 88H, 99H, 0aaH, 55H, 0ccH, 0ddH, 0eeH, 0ffH
+mydata ends
+
+mycode segment
+START:
+    assume ds:mydata
+    mov ax, mydata
+    mov ds, ax
+
+    mov ax, mydata
+    mov es, ax
+
+    lea si, g_szStr1
+    lea di, g_szStr2
+
+    mov cx, offset g_szStr2 - offset g_szStr1
+    
+    rep movsb
+
+    mov ax, 4200H
+    int 21H
+mycode ends
+end START
